491e306e1bfa652836231d4cb754471bd3717ea7
Bernard Maltais <bmaltais@gmail.com>
2020-06-26 18:00:49 +0000

    Fix outputs

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,28 @@
 output "name" {
-  value = azurerm_linux_virtual_machine.VM.name
+  depends_on = [azurerm_linux_virtual_machine.VM[0]]
+
+  value = azurerm_linux_virtual_machine.VM[0].name
 }
 
 output "id" {
-  value = azurerm_linux_virtual_machine.VM.id
+  depends_on = [azurerm_linux_virtual_machine.VM[0]]
+
+  value = azurerm_linux_virtual_machine.VM[0].id
 }
 
 output "vm" {
+  depends_on = [azurerm_linux_virtual_machine.VM]
+
   value = azurerm_linux_virtual_machine.VM
 }
 
 output "pip" {
+  depends_on = [ azurerm_public_ip.VM-EXT-PubIP ]
+
   value = azurerm_public_ip.VM-EXT-PubIP
 }
 
 output "nic" {
+  depends_on = [azurerm_network_interface.NIC]
   value = azurerm_network_interface.NIC
 }
