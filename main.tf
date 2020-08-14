c2ec97c723ee2eda961ead1257b47400f8d7dcb6
Bernard Maltais <bmaltais@gmail.com>
2020-08-14 13:05:56 +0000

    Update

--- main.tf
+++ main.tf
@@ -75,9 +75,11 @@ resource azurerm_network_interface NIC {
 }
 
 resource azurerm_network_interface_backend_address_pool_association LB {
-  network_interface_id    = azurerm_network_interface.NIC.id
+  for_each = toset(var.load_balancer_backend_address_pools_ids)
+
+  network_interface_id    = var.deploy ? azurerm_network_interface.NIC[0].id : null
   ip_configuration_name   = "ipconfig1"
-  backend_address_pool_id = var.load_balancer_backend_address_pools_ids
+  backend_address_pool_id = each.key
 }
 
 resource azurerm_network_interface_application_security_group_association asg {
