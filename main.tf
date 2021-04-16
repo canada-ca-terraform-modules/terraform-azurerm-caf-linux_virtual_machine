ac0ceedbee8d4559723e676a3a4097139b5e543b
Bernard Maltais <bernard.maltais@canada.ca>
2021-04-16 08:27:37 -0400

    Azzing zone to lifecycle

--- main.tf
+++ main.tf
@@ -161,7 +161,7 @@ resource azurerm_linux_virtual_machine VM {
       admin_password,
       identity,
       os_disk, # Prevent restored OS disks from causinf terraform to attempt to re-create the original os disk name and break the restores OS
-      private_ip_addresses[0],
+      zone,
     ]
   }
 }
